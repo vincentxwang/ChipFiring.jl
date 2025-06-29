5e44938971fa9be67983467ca8021c8662fa49d6
Vincent Wang <vw12@rice.edu>
2025-06-29 13:11:19 -0400

    cleanup

--- ChipFiring/src/utils.jl
+++ ChipFiring/src/utils.jl
@@ -1,6 +1,5 @@
 """
-
-generate_effective_divisors(num_vertices, degree) -> Vector{Divisor}
+    generate_effective_divisors(num_vertices, degree) -> Vector{Divisor}
 
 
 Generates all effective divisors (chip configurations with non-negative chips)
@@ -39,15 +38,15 @@ function has_rank_at_least_r(g::ChipFiringGraph, d::Divisor, r::Int, cgon::Bool)
 end
 
 """
- subdivide(G::ChipFiringGraph, subdivisions::Int)
+    subdivide(G::ChipFiringGraph, subdivisions::Int)
 
- Given a ChipFiring object G, produces another ChipFiring object which is an n-uniform subdivision of G.
+Given a ChipFiring object G, produces another ChipFiring object which is an n-uniform subdivision of G.
 
- # Arguments
- - `G::ChipFiringGraph` the original Graph
- - `subdivisions::Int` number of subdivisions (1 returns original graph, 2 produces 2-uniform subdivision, etc)
+# Arguments
+- `G::ChipFiringGraph` the original Graph
+- `subdivisions::Int` number of subdivisions (1 returns original graph, 2 produces 2-uniform subdivision, etc)
 
- # Returns subdivided graph
+# Returns subdivided graph
 """
 function subdivide(G::ChipFiringGraph, subdivisions::Int)
     # if no subdivisions 
@@ -81,7 +80,7 @@ end
 end
 
 """
-    toGraphJL(g::ChipFiringGraph)
+    to_graphjl(g::ChipFiringGraph)
 
 Converts a `ChipFiringGraph` into a `Graphs.Graph` object for use with the
 Graphs.jl library.
@@ -95,7 +94,7 @@ in the `ChipFiringGraph` are ignored.
 # Returns
 - `Graphs.Graph`: A simple graph representation of `g`.
 """
-function toGraphJL(g::ChipFiringGraph)
+function to_graphjl(g::ChipFiringGraph)
     # Create a new simple graph with the same number of vertices
     jl_graph = SimpleGraph(g.num_vertices)
     
