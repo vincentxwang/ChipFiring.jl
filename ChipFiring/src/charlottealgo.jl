0c51f36b0056793707a21cb8cfb474da5bf0dff0
Charlotte <cjc11@williams.edu>
2025-07-10 14:42:42 -0400

    more graphs i think

--- ChipFiring/src/charlottealgo.jl
+++ ChipFiring/src/charlottealgo.jl
@@ -20,6 +20,7 @@ end
 function plot_gon_sequence(g::ChipFiringGraph, maxd::Int, maxr::Int)
     r_values = 1:maxr
     gon_values = [compute_gonality(g, r = rval, verbose = false, max_d = maxd) for rval in r_values]
+    genus = g.num_edges - g.num_vertices + 1
 
     scatter(r_values, gon_values,
     xlabel = "r",
@@ -28,16 +29,17 @@ function plot_gon_sequence(g::ChipFiringGraph, maxd::Int, maxr::Int)
     legend = false,
     markerstrokewidth = 0.5,
     markersize = 6,
-    color = :red
-)
+    color = :red,
+    )
+    vline!([genus])
 end
 
 function plot_both(g::ChipFiringGraph, max::Int, highR::Int)
     r_values = 1:highR
     cgon_values = [compute_gonality(g, r = rval, cgon = true, verbose = false, max_d=max) for rval in r_values]
     gon_values = [compute_gonality(g, r = rval, verbose = false, max_d = max) for rval in r_values]
-
-    scatter(r_values, cgon_values, grid = "on", xticks=0:highR, yticks=0:max,
+    genus = g.num_edges - g.num_vertices + 1
+    scatter(r_values, cgon_values, grid = true, xticks=0:highR, yticks=0:max,
     xlabel = "r",
     ylabel = "cgon_r",
     title = "Scatter Plot of Function Output",
@@ -48,13 +50,15 @@ function plot_both(g::ChipFiringGraph, max::Int, highR::Int)
     )
 
     
-    scatter!(r_values, gon_values,
-    xlabel = "r",
+    scatter!(r_values, gon_values, grid = true,
+    xlabel = "r", 
     ylabel = "gon_r",
     title = "Scatter Plot of Function Output",
     legend = false,
     markerstrokewidth = 0.5,
     markersize = 4,
     color = :red
-)
+    )
+    vline!([genus])
+    vline!()
 end
--- ChipFiring/test/gonality_savings_graphs.jl
+++ ChipFiring/test/gonality_savings_graphs.jl
@@ -47,7 +47,7 @@ using Test
         @test compute_gonality(s2, max_d=5, verbose=true) == 4
     end
 
-    @testset "Ralph's Tricycleish 5 to 4"
+    @testset "Ralph's Tricycleish 5 to 4" 
         num_vertices = 7
         edge_list = [(1,2), (1,6), (1,6), (1,6), (1,7), (2,3), (2,3), (2,3), (3,4), (3,7), (4,5), (4,5), (4,5), (5,6), (5,7)]
         g = ChipFiringGraph(num_vertices, edge_list)
@@ -58,6 +58,14 @@ using Test
         @test compute_gonality(s2, max_d=5, verbose=true) == 4
    
     end 
+
+    @testset "Charlotte's web" begin
+        num_vertices = 11
+        edge_list = [(1,2), (1,3), (1,4), (1,5), (1,6), (1,7),(3,2), (2,7), (2,8), (3,4), (3,9), (4,5), (4,9), (4,11), (5,6), (5,10), (5,11), (6,7), (6,10), (7,8)]
+        g = ChipFiringGraph(num_vertices, edge_list)
+
+    end
+
 
 @testset "Misc Graphs" begin
 
@@ -175,26 +183,52 @@ using Test
         g = ChipFiringGraph(adj_matrix)
     end
     @testset "C5" begin
-            adj_matrix = [
-                        0 1 0 0 1;
-                        1 0 1 0 0;
-                        0 1 0 1 0;
-                        0 0 1 0 1;
-                        1 0 0 1 0;
-            ]
-            g = ChipFiringGraph(adj_matrix)
+        adj_matrix = [
+                    0 1 0 0 1;
+                    1 0 1 0 0;
+                    0 1 0 1 0;
+                    0 0 1 0 1;
+                    1 0 0 1 0;
+        ]
+        g = ChipFiringGraph(adj_matrix)
     end
 
     #Random Graphs
     @testset "House" begin
-            adj_matrix = [
-                        0 1 0 0 1;
-                        1 0 1 0 1;
-                        0 1 0 1 0;
-                        0 0 1 0 1;
-                        1 1 0 1 0;
-            ]
-            g = ChipFiringGraph(adj_matrix)
+        adj_matrix = [
+                    0 1 0 0 1;
+                    1 0 1 0 1;
+                    0 1 0 1 0;
+                    0 0 1 0 1;
+                    1 1 0 1 0;
+        ]
+        g = ChipFiringGraph(adj_matrix)
+    end
+
+    @testset "Weird house minus an edge" begin
+    
+        #  x--- x --- x ---x
+        #         \ /
+        #          x
+        adj_matrix = [
+                    0 1 0 0 2;
+                    1 0 3 0 1;
+                    0 3 0 0 0;
+                    0 0 0 0 3;
+                    2 1 0 3 0;
+        ]
+        g = ChipFiringGraph(adj_matrix)
+    end
+
+    @testset "kissing triangles" begin
+        adj_matrix = [
+                    0 1 1 0 0;
+                    1 0 1 0 0;
+                    1 1 0 1 1;
+                    0 0 1 0 1;
+                    0 0 1 1 0;
+        ]
+        g = ChipFiringGraph(adj_matrix)
     end
 
     @testset "Shell-3" begin
@@ -231,5 +265,32 @@ using Test
             ]
             g = ChipFiringGraph(adj_matrix)
     end
+
+@testset "Line like graphs" begin
+    @testset "Line like 1,2" begin
+            adj_matrix = [
+                        0 1 0 0 1 0 0;
+                        1 0 1 0 1 0 0;
+                        0 1 0 1 1 0 0;
+                        0 0 1 0 1 1 0;
+                        1 1 1 1 0 1 1;
+                        0 0 0 1 1 0 1;
+                        0 0 0 0 1 1 0;
+            ]
+            g = ChipFiringGraph(adj_matrix)
+    end
+end
+
+@testset "Line like graphs" begin
+    @testset "Line like 1,2" begin
+        edge_list = [(1,2),(1,2),(1,3),(2,3),(3,4),(4,5),(4,6),(1,9),(2,7),(10,8),(5,9),(9,10),(9,10),(5,10),(6,8),(6,7),(7,8), (1,11), (11,12), (11,16), (2,12), (12,13), (13,7), (13, 14), (14, 8), (14,15), (15,10), (15,16), (16,9)]
+        # sample usage 
+        g = ChipFiringGraph(16, edge_list)
+
+    end
+end
+
+
+        
 
 end
