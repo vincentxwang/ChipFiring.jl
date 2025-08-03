a37da354cc6af078dfb3268d42c4c00dd1381d70
Vincent Wang <vw12@rice.edu>
2025-08-03 17:48:10 -0400

    maybe this time

--- docs/make.jl
+++ docs/make.jl
@@ -2,7 +2,6 @@ using Documenter, ChipFiring
 
 makedocs(;
     modules=[ChipFiring],
-    authors="Vincent X. Wang <vw12@rice.edu>",
     sitename="ChipFiring.jl",
     format=Documenter.HTML(;
         canonical="https://vincentxwang.github.io/ChipFiring.jl",
@@ -16,5 +15,6 @@ makedocs(;
 )
 
 deploydocs(
-    repo = "github.com/vincentxwang/ChipFiring.jl.git",
+    repo = "github.com/vincentwang/ChipFiring.jl",
+    devbranch = "main"
 )
